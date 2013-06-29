aa416763bda75ad08ad74343a141099bf4594661
Matias Linares <matiaslina@gmail.com>
2013-06-28 21:15:48 -0300

    RunProgram funciton, needed for the tester

--- libs/compiler.go
+++ libs/compiler.go
@@ -5,19 +5,35 @@ import (
     "log"
     "fmt"
     "os/exec"
-    //"strings"
+    "strings"
 )
 
 type Compiler struct {
     Executable  string
     Gcc         *exec.Cmd
+    Program     *exec.Cmd
 }
 
 func compile (source string, executable string) *Compiler {
     return &Compiler {
         Executable: executable,
         Gcc: exec.Command("gcc", "-Wall","-Werror","-Wextra","-o " + executable,source),
+        Program: nil,
     }
+}
+
+func (c *Compiler) RunProgram (channelOut chan string, input string) {
+    var out bytes.Buffer
+    c.Program = exec.Command (c.Executable)
+    c.Program.Stdout = &out
+    c.Program.Stdin = strings.NewReader (input)
+
+    err := c.Program.Run()
+    if err != nil {
+        channelOut <- err.Error()
+        return
+    }
+    channelOut <- out.String()
 }
 
 func main () {
