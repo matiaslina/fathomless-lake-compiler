3d223164da2e4302f52227a10873e36e29c0e095
Matias Linares <matiaslina@gmail.com>
2013-07-01 14:29:45 -0300

    Ahora se pueden subir archivos :) espero que ande bennnnn

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
     "log"
     "sheltered-inlet/tester"
     "fmt"
+    "io/ioutil" 
 )
 
 var (
@@ -17,12 +18,28 @@ var (
 
 func init () {
     http.HandleFunc ("/", RootHandler)
+    http.HandleFunc ("/submitted", SubmittedHandler)
 }
 
 func RootHandler (w http.ResponseWriter, req *http.Request) {
     err := RootTemp.Execute (w, listenAddr)
     if err != nil {
         http.Error (w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func SubmittedHandler (w http.ResponseWriter, req *http.Request) {
+    file, handler, err := req.FormFile ("file")
+    if err != nil {
+        fmt.Println("[step 1]Oh.. we have an error with the file :/")
+    }
+    data, err := ioutil.ReadAll (file)
+    if err != nil {
+        fmt.Println("[step 2]Oh.. we have an error with the file :/")
+    }
+    err = ioutil.WriteFile (handler.Filename, data, 0755)
+    if err != nil {
+        fmt.Println ("[step 3] Oh.. can't set the file in the disk :/")
     }
 }
 
